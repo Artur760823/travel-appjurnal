34d826029dd3975e30be830c421f9f032b6ae3f9
Artur <artur.zajaczkowski76@gmail.com>
2022-02-15 11:57:31 +0100

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import './Card.css';
 
 function Card({ item }) {
   return (
     <div className='card'>
       <img className='card__img' src={item.image} />
-      <div className='card__country'>{item.country}</div>
-      <div className='card__localization'>{item.localization}</div>
-      <div className='card__date'>{item.date}</div>
-      <div className='card__description'>{item.description}</div>
+      <div className='card__notes'>
+        <div className='card__country'>{item.country}</div>
+        <div className='card__localization'>{item.localization}</div>
+        <div className='card__date'>{item.date}</div>
+        <div className='card__description'>{item.description}</div>
+      </div>
+
     </div>
   )
 }
